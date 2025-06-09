6cc3fc2430e0524a1590a770ca9c3cafe62beb68
benjamin <benjaminhucko@gmail.com>
2025-06-09 15:41:14 +0200

    example script structure improved

--- parametric_reconstruction_example.py
+++ parametric_reconstruction_example.py
@@ -9,25 +9,26 @@ import logging
 
 from visualize import plot_embeddings
 
-data = load_digits()
-digits = data.data
-key = random.PRNGKey(42)
-original_shape = digits.shape[0], 8, 8
+if __name__ == '__main__':
+    data = load_digits()
+    digits = data.data
+    key = random.PRNGKey(42)
+    original_shape = digits.shape[0], 8, 8
 
-# Configure logging to show INFO level messages and above
-logging.basicConfig(
-    level=logging.INFO,  # Set the logging level to INFO
-    format='%(asctime)s - %(levelname)s - %(message)s'  # Format of the log messages
-)
-embedding, model, params = ptrimap.fit_transform(digits, 2, key, verbose=True)
-plot_embeddings(embedding, data.target)
+    # Configure logging to show INFO level messages and above
+    logging.basicConfig(
+        level=logging.INFO,  # Set the logging level to INFO
+        format='%(asctime)s - %(levelname)s - %(message)s'  # Format of the log messages
+    )
+    embedding, model, params = ptrimap.fit_transform(digits, 2, key, verbose=True)
+    plot_embeddings(embedding, data.target)
 
-reconstructed_digits = ptrimap.inverse_transform(embedding, model, params)
-reconstructed_digits = reconstructed_digits.reshape(original_shape)
-digits = digits.reshape(original_shape)
-fig, axs = plt.subplots(3, 2)
-for i in range(3):
-    axs[i][0].imshow(digits[i], cmap='gray')
-    axs[i][1].imshow(reconstructed_digits[i], cmap='gray')
+    reconstructed_digits = ptrimap.inverse_transform(embedding, model, params)
+    reconstructed_digits = reconstructed_digits.reshape(original_shape)
+    digits = digits.reshape(original_shape)
+    fig, axs = plt.subplots(3, 2)
+    for i in range(3):
+        axs[i][0].imshow(digits[i], cmap='gray')
+        axs[i][1].imshow(reconstructed_digits[i], cmap='gray')
 
-plt.show()
+    plt.show()
